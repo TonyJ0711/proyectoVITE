f6e94f988a3361f19ccc96b0a9f1e22b5b33afec
Joel <tuemail@example.com>
2025-10-28 12:57:15 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="container text-center mt-5">
       <h1 className="text-primary">👋 ¡Hola A TODOS CON BOOTSTRAP!</h1>
-      <p className="lead mt-3">WASAAAAAAAAAAAAAA2</p>
+      <p className="lead mt-3">WASAAAAAAAAAAAAAA</p>
       <button className="btn btn-success mt-3">Haz clic aquí para conocer todo </button>
     </div>
   );
